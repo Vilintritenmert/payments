9979f7866ef1d2d3d6ed5ecd00e0b024d4d50d9a
victor <vilintritenmert@gmail.com>
2017-08-07 03:31:14 +0300

    fix bug

--- library.php
+++ library.php
@@ -1,18 +1,28 @@
 <?php
 
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
 class Sales
 {
     public function testPayment(array $data, $amount)
     {
         $payment = new PaypalPayment;
         try {
-            $payment->validate($data, function ($testAmount) use ($amount, $payment) {
-                return $payment->test($amount, $testAmount);
+            $payment->validate($data, function ($testAmount) use ($amount) {
+                return $this->test($amount, $testAmount);
             });
         } catch (\PaypalException $e) {
             new \RuntimeException('something weird', 23, $e);
         }
+
         return $payment;
+    }
+
+    public function test($amount, $testAmount)
+    {
+        return $amount === $testAmount;
     }
 }
 
@@ -20,17 +30,13 @@ class PaypalPayment
 {
     private $testAmount = 200;
 
-    public function validate(array $data, callable $comparator)
+    public function validate(array $data, closure $comparator)
     {
         if (!$comparator($this->testAmount)) {
             throw new \PaypalException("Incorrect amount");
         }
     }
 
-    public function test($amount, $testAmount)
-    {
-        return $amount === $testAmount;
-    }
 }
 
 
